b0230d362c624cfbaae5fb20a2b739899d8a597e
Leandro Tabaj <tabajleandro@gmail.com>
2022-04-17 20:59:55 -0300

    wip

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from booking.booking import Booking
+import booking.constants as const
+import time
+
+cities = const.CITIES
 
 with Booking(teardown=True) as bot:
     bot.land_first_page()
+    
+    select = bot.find_element_by_id('form')
+    options = select.find_elements_by_tag_name('option')
+    time.sleep(2)
+    for option in options:
+        print(option.get_attribute('value') + ' => ' + option.text)
+        option.click()
+        time.sleep(1)
+
